5554ca04e0e0b66814bc268605ec4aa6615858a9
purvansh <purvansh@gmail.com>
2021-10-18 18:14:20 +0530

    New thing

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,15 +1,15 @@
-from flask import  Flask
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
-app.config['SECRET_KEY']='364172c4502c6c035b0d3070b54e4002'
-db=SQLAlchemy(app)
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
-login_manager=LoginManager(app)
-login_manager.login_view =  'login'  # the login is the name of the function of the login function that we createed in routes.py
-login_manager.login_message_category= 'info'
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 
 from flaskblog import routes
